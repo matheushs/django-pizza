09f0eb07325eb03b2deb9c2e2fe8ecd3c75b4b27
Matheus Henrique Soares <matt.h.soares@gmail.com>
2015-08-18 09:42:38 -0300

    Omg new pizza order

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -6,3 +6,11 @@ if sauce.startswith('toma'):
     print("It's probably tomato...")
 else:
     print("not tomato")
+
+order_description = """
+Ayy lmao look at that normfag ordering ham pizza.
+Gimme my pepperonis you noob.
+"""
+
+if(order_description):
+    print(order_description)
