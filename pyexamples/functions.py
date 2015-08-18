be41841fe1398b1651924a8fc6716cf9282cfec1
Matheus Henrique Soares <matt.h.soares@gmail.com>
2015-08-18 11:39:23 -0300

    Omg im the python mastah

--- pyexamples/functions.py
+++ pyexamples/functions.py
@@ -1,14 +1,21 @@
 orders = []
 
 
-def add_order(name, flavor):
+def add_order(name, flavor, observation=None):
     order = {}
     order['name'] = name
     order['flavor'] = flavor
+    order['observation'] = observation
+    return order
 
-    orders.append(order)
 
-print(orders)
-add_order('Mario', 'pepperoni')
-add_order('Pirao', 'rola')
-print(orders)
+orders.append(add_order('Mario', 'Pepperoni'))
+orders.append(add_order('Pirao', 'Rola', 'Prefiro pegar o Ordonha'))
+
+for order in orders:
+    if order['observation']:
+        template = 'Name: {name}\nFlavor: {flavor}\nObservation: {observation}'
+    else:
+        template = 'Name: {name}\nFlavor: {flavor}'
+    print(template.format(**order))
+    print('-'*30)
