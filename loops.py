95a8e873700c7b755382e42bf242890a93d9aa03
Matheus Henrique Soares <matt.h.soares@gmail.com>
2015-08-18 10:50:25 -0300

    Omg looping pizzas

---
+++ loops.py
@@ -0,0 +1,13 @@
+orders = [ 
+    {
+        'name': 'Mario',
+        'flavor': 'Pepperoni'
+    },
+    {
+        'name': 'Pirao',
+        'flavor': 'Rola'
+    }
+]
+
+for order in orders:
+    print(order['name'], order['flavor'])
